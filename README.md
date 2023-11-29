a9532162c5818e80c8f197f0f9431ebda2a9f3d1
sharadhi-hr <sharadhihrsharadhi@gmail.com>
2023-11-29 02:37:44 -0800

    two files updated

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # cseGIT
 this is the readme of cseGIT repository
 sharadhi h r
+added a newline at 4pm
--- first.java
+++ first.java
@@ -3,5 +3,6 @@ class first
     public static void main(String args[])
     {
       System.out.println("welcome to GIT");
+      //this is comment line
     }
   }
